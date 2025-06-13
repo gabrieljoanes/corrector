f5d59a6b7617977901c1cfbcd18d2d6fac9e280b
gabrieljoanes <128034350+gabrieljoanes@users.noreply.github.com>
2025-06-13 05:37:09 +0300

    Update highlight_renderer.py

--- modules/highlight_renderer.py
+++ modules/highlight_renderer.py
@@ -8,14 +8,13 @@ def render_highlighted_text(original_text, corrections, decisions, focus_idx=Non
         st.markdown(original_text)
         return
 
-    # Sort corrections in reverse order to not break offsets
     sorted_corrections = sorted(
         [(i, c) for i, c in enumerate(corrections) if decisions.get(i) != "rejected"],
         key=lambda x: x[1]["start_idx"],
         reverse=True
     )
 
-    text = original_text  # Don't escape yet
+    text = original_text
 
     for i, corr in sorted_corrections:
         start = corr["start_idx"]
@@ -26,18 +25,18 @@ def render_highlighted_text(original_text, corrections, decisions, focus_idx=Non
         mistake = text[start:end]
         correction = corr["after"]
 
+        label = f"<span style='font-size: 0.8em; color: gray;'>[# {i+1}]</span>"
+
         before_html = f"<span style='color: red; text-decoration: line-through;'>{mistake}</span>"
         after_html = f"<span style='color: green; font-weight: bold;'>{correction}</span>"
 
         if i == focus_idx:
             wrapper_style = "background-color: #fff3cd; padding: 4px 6px; border-radius: 4px;"
-            replacement = f"<span style='{wrapper_style}'>{before_html}&nbsp;&nbsp;{after_html}</span>"
+            replacement = f"<span style='{wrapper_style}'>{before_html}&nbsp;&nbsp;{after_html}&nbsp;&nbsp;{label}</span>"
         else:
-            replacement = f"{before_html}&nbsp;&nbsp;{after_html}"
+            replacement = f"{before_html}&nbsp;&nbsp;{after_html}&nbsp;&nbsp;{label}"
 
-        # Add surrounding spaces for clarity
         replacement = f" {replacement} "
         text = text[:start] + replacement + text[end:]
 
-    # Escape only final HTML-safe version
     st.markdown(text, unsafe_allow_html=True)
